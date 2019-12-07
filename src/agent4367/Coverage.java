2f4b700ee6fe8473432abab4e3cd0b09c26d1704
AdrianDizon <amd160530@utdallas.edu>
2019-12-07 01:12:03 -0600

    Update Coverage.java
    Removed the try and catch statements and just added a "throws Exception" to the method to make it more uniform to how similar situations are handled elsewhere in the code

--- src/agent4367/Coverage.java
+++ src/agent4367/Coverage.java
@@ -13,26 +13,19 @@ public class Coverage {
 		//System.out.println("String added to hash");
 	}
 	
-	public static void writeToFile(FileWriter writer)
+	public static void writeToFile(FileWriter writer) throws Exception
 	{
-		try 
-		{		  
-			//initializes a hash iterator and a buffer string to put the hash elements
-		    Iterator<String> iterator = hash.iterator();  
-            String buffer = "";		
+		//initializes a hash iterator and a buffer string to put the hash elements
+		Iterator<String> iterator = hash.iterator();  
+		String buffer = "";		
             
-            //the strings in the hash are added to the iterator along with a newline
-            while (iterator.hasNext())
-                buffer += iterator.next() + System.lineSeparator();  
+		//the strings in the hash are added to the iterator along with a newline
+		while (iterator.hasNext())
+			buffer += iterator.next() + System.lineSeparator();  
             
-            //once the iterator goes through the entire hash set, the writer writes the buffer into tmt-cov.txt and the hash is cleared
-			writer.write(buffer);	
-			System.out.println("written to file");
-			hash.clear();
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();	
-		}
+		//once the iterator goes through the entire hash set, the writer writes the buffer into stmt-cov.txt and the hash is cleared
+		writer.write(buffer);	
+		//System.out.println("written to file");
+		hash.clear();
 	}		
 }
