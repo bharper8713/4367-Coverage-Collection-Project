02787688fa180c8b270b31d4957aea0615898b28
AdrianDizon <amd160530@utdallas.edu>
2019-12-08 13:55:02 -0600

    Update Coverage.java
    Updated for Phase 2 stuff

--- src/agent4367/Coverage.java
+++ src/agent4367/Coverage.java
@@ -5,12 +5,19 @@ import java.util.*;
 
 public class Coverage {
 	static HashSet<String> hash = new HashSet<String>();
+	static HashSet<String> hashVars = new HashSet<String>();
 		
 	public static void visitedLine(String string) 
 	{
 		//adds the string from MethodVisitor4367
 		hash.add(string);
-		//System.out.println("String added to hash");
+	}
+	
+	//PHASE 2
+	public static void visitedVar(String string) 
+	{
+		//adds the string from MethodVisitor4367
+		hashVars.add(string);
 	}
 	
 	public static void writeToFile(FileWriter writer) throws Exception
@@ -27,5 +34,23 @@ public class Coverage {
 		writer.write(buffer);	
 		//System.out.println("written to file");
 		hash.clear();
-	}		
+	}
+	
+	//PHASE 2
+	//Writes the variables to the file specified
+	public static void writeVarsToFile(FileWriter writer) throws Exception
+	{
+		//initializes a hash iterator and a buffer string to put the hash elements
+		Iterator<String> iterator = hashVars.iterator();  
+		String buffer = "";		
+            
+		//the strings in the hash are added to the iterator along with a newline
+		while (iterator.hasNext())
+			buffer += iterator.next() + System.lineSeparator();  
+            
+		//once the iterator goes through the entire hash set, the writer writes the buffer into stmt-cov.txt and the hash is cleared
+		writer.write(buffer);	
+		//System.out.println("written to file");
+		hashVars.clear();
+	}
 }
